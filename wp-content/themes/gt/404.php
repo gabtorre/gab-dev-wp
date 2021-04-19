082fdc7a0d638c47082374cf1a446aac6e4fd2fd
gabtorre <gtorresx3@gmail.com>
2021-04-19 16:09:11 -0700

    add theme

---
+++ wp-content/themes/gt/404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="container-404">
+    <h2 class="page-heading">Oh! What? 404?</h2>
+    <img src="http://source.unsplash.com/640x480/?cats">
+
+    <h3>Sorry frnd, I think you're lost. Plz try the follin links.</h3>
+
+    <ul>
+        <li><a href="<?php echo site_url('/blog') ?>">Blog List</a></li>
+        <li><a href="<?php echo site_url('/projects') ?>">Projects List</a></li>
+        <li><a href="<?php echo site_url('/about') ?>">About</a></li>
+        <li><a href="<?php echo site_url('/') ?>">Home Page</a></li>
+    </ul>
+
+</div>
+
+<?php get_footer(); ?>
